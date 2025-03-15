e2ba0237c32892e8ab83287aa810a5802cb56199
Victor Gomes <victorgomeslucio1@hoymail.com>
2025-03-15 13:39:17 -0300

    update

---
+++ mod-15/abstractfactory/bike_category/Sport.java
@@ -0,0 +1,22 @@
+package edu.java.abstractfactory.bike_category;
+import edu.java.abstractfactory.abstracts.Bike;
+import edu.java.abstractfactory.abstracts.Factory;
+import edu.java.abstractfactory.bikes.S1000rr;
+/** @author gomesvh
+ * A classe que representa a categoria de motos esportivas.
+ */
+public class Sport extends Factory {
+    /**
+     * Recupera uma moto esportiva com base na categoria solicitada.
+     *
+     * @param requested A categoria da moto esportiva solicitada.
+     * @return Uma instância da moto esportiva recuperada, ou null se a categoria não for reconhecida.
+     */
+    @Override
+    public Bike retrieveBike(String requested) {
+        if ("Sport".equalsIgnoreCase(requested)) {
+            return new S1000rr("Vermelha");
+        }
+        return null;
+    }
+}
